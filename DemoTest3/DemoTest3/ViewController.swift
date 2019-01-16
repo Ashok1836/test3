60d2e8a2e52de4321663bfc991c6c72784d66cbd
Ashok Reddy <ashokreddy@ls-phy-lap-0113.local>
2019-01-16 12:36:31 +0530

    first version of demotest3

---
+++ DemoTest3/DemoTest3/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  DemoTest3
+//
+//  Created by Ashok Reddy on 16/01/19.
+//  Copyright © 2019 Ashok Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("This is Demo Test3")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
